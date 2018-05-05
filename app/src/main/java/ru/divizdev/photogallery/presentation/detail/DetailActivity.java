98e19d527d0628ac612455d88f44c368e5fc1068
diviz <diviz.dev@gmail.com>
2018-05-05 14:23:30 +0300

    add ZoomageView

--- app/src/main/java/ru/divizdev/photogallery/presentation/detail/DetailActivity.java
+++ app/src/main/java/ru/divizdev/photogallery/presentation/detail/DetailActivity.java
@@ -4,9 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
-import android.widget.ImageView;
+
+import com.jsibbold.zoomage.ZoomageView;
 
 import ru.divizdev.photogallery.GlideApp;
 import ru.divizdev.photogallery.R;
@@ -14,11 +13,9 @@ import ru.divizdev.photogallery.entities.ImageUI;
 
 public class DetailActivity extends AppCompatActivity implements IDetailView {
 
-    private ImageView _imageView;
+    private ZoomageView _imageView;
     private DetailPresenter _detailPresenter = DetailPresenter.getInstance();
 
-    private ScaleGestureDetector _scaleGestureDetector;
-    private float _scaleFactor = 1.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +23,7 @@ public class DetailActivity extends AppCompatActivity implements IDetailView {
         setContentView(R.layout.activity_detail);
 
         _imageView = findViewById(R.id.image_detail);
-        _scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+
 
         _detailPresenter.attachView(this);
     }
@@ -42,23 +39,5 @@ public class DetailActivity extends AppCompatActivity implements IDetailView {
         GlideApp.with(this).load(image.getDetailImageUrl()).centerInside().into(_imageView);
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        _scaleGestureDetector.onTouchEvent(ev);
-
-        return true;
-    }
-
-    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-        @Override
-        public boolean onScale(ScaleGestureDetector scaleGestureDetector){
-            _scaleFactor *= scaleGestureDetector.getScaleFactor();
-            _scaleFactor = Math.max(1f,
-                    Math.min(_scaleFactor, 10.0f));
-            _imageView.setScaleX(_scaleFactor);
-            _imageView.setScaleY(_scaleFactor);
-            return true;
-        }
-    }
 
 }
