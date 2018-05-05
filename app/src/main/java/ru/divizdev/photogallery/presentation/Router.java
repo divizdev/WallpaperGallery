bebdcba9985300bf7fd40d54878d630a2cd3c4d1
diviz <diviz.dev@gmail.com>
2018-05-05 23:54:59 +0300

    new icon launcher

--- app/src/main/java/ru/divizdev/photogallery/presentation/Router.java
+++ app/src/main/java/ru/divizdev/photogallery/presentation/Router.java
@@ -1,6 +1,5 @@
 package ru.divizdev.photogallery.presentation;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
@@ -12,25 +11,6 @@ import ru.divizdev.photogallery.presentation.detail.DetailActivity;
 public class Router {
 
 
-    public void navTo(Context context, Screen screen) {
-        Intent intent = null;
-
-        switch (screen) {
-            case list:
-                break;
-            case detail:
-
-                break;
-            case about:
-                break;
-        }
-        if (intent != null) {
-
-            context.startActivity(intent);
-        }
-
-    }
-
     public void navToDetail(@NonNull AppCompatActivity activity, Integer id){
        Intent intent = DetailActivity.newIntent(activity, id);
        activity.startActivity(intent);
@@ -41,13 +21,6 @@ public class Router {
         FragmentManager fragmentManager = activity.getSupportFragmentManager();
         AboutDialog aboutDialog = new AboutDialog();
         aboutDialog.show(fragmentManager, "");
-    }
-
-
-    public enum Screen {
-        list,
-        detail,
-        about
     }
 
 }
